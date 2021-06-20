d8a8a160b836817cb40815e1d6e3de0f5d433f69
Yuki Onishi <yuki.st16@gmail.com>
2021-06-21 04:42:56 +0900

    Complete stabilization algorithms

--- sample/Jaxon2WalkController.cpp
+++ sample/Jaxon2WalkController.cpp
@@ -61,20 +61,34 @@ class Jaxon2WalkController : public SimpleController
     int currentFrameIndex;
     double dt;
     std::vector<double> q_old, qref_old;
-    Vector3 bodyModification, dZmp;
+    std::vector<double> qref_modified;
+    Vector3 bodyModification;
 
     shared_ptr<MultiValueSeq> qseq;
     shared_ptr<Vector3Seq> zmpseq;
 
 public:
-    virtual bool initialize(SimpleControllerIO* io) override
+    virtual bool initialize(SimpleControllerIO *io) override
     {
         // initializes variables
         currentFrameIndex = 0;
         dt = io->timeStep();
         ioBody = io->body();
-        q_old.resize(ioBody->numJoints(), 0.0);
-        qref_old.resize(ioBody->numJoints(), 0.0);
+        q_old.reserve(ioBody->numJoints());
+        qref_old.reserve(ioBody->numJoints());
+        qref_modified.reserve(ioBody->numJoints());
+        for (auto joint : ioBody->joints()) {
+            const double q = joint->q();
+            q_old.push_back(q);
+            qref_old.push_back(q);
+            qref_modified.push_back(q);
+            // should be executed in when simulation starts
+        }
+        bodyModification = Vector3::Zero();
+
+        // to calculate ZMP in the global coordinate
+        io->enableInput(ioBody->link("RLEG_LINK5"), LINK_POSITION);
+        io->enableInput(ioBody->link("LLEG_LINK5"), LINK_POSITION);
 
         // registers force sensors
         rf_sensor = ioBody->findDevice<ForceSensor>("RF_SENSOR");
@@ -83,7 +97,7 @@ public:
         // prepares a virtual body used in FK/IK
         ikBody = ioBody->clone();
 
-        for(auto joint : ioBody->joints()) {
+        for (auto joint : ioBody->joints()) {
             joint->setActuationMode(Link::JOINT_TORQUE);
             io->enableIO(joint);
         }
@@ -100,89 +114,85 @@ public:
         baseToLAnkle->calcForwardKinematics();
 
         // loads reference trajectories
-        auto path = shareDirPath() / "JAXON2" / "motion" / "JAXON2" / "SampleWalkPattern.seq";
+        auto path = shareDirPath() / "JAXON2" / "motion" / "JAXON2"
+                    / "SampleWalkPattern.seq";
         BodyMotion motion;
-        if(!motion.loadStandardYAMLformat(path.string())) {
+        if (!motion.loadStandardYAMLformat(path.string())) {
             io->os() << motion.seqMessage() << endl;
             return false;
         }
         qseq = motion.jointPosSeq();
-        if(qseq->numFrames() == 0){
+        if (qseq->numFrames() == 0) {
             io->os() << "Empty motion data." << endl;
             return false;
         }
-        if(qseq->numParts() != ioBody->numJoints()) {
-            io->os() << "Mismatch between the robot and the motion data regarding the number of joints" << endl;
+        if (qseq->numParts() != ioBody->numJoints()) {
+            io->os() << "Mismatch between the robot and the motion data "
+                        "regarding the number of joints"
+                     << endl;
             return false;
         }
         zmpseq = motion.extraSeq<Vector3Seq>("ZMPSeq");
-        if(!zmpseq || zmpseq->numFrames() == 0) {
+        if (!zmpseq || zmpseq->numFrames() == 0) {
             io->os() << "A valid ZMP seq is not available." << endl;
             return false;
         }
-        if(fabs(io->timeStep() - qseq->timeStep()) > 1.0e-6) {
-            io->os() << "Warning: the simulation time step is different from that of the motion data " << endl;
+        if (fabs(io->timeStep() - qseq->timeStep()) > 1.0e-6) {
+            io->os() << "Warning: the simulation time step is different from "
+                        "that of the motion data "
+                     << endl;
         }
 
         return true;
     }
 
-    void calculateBodyModification(const Vector3 &refZmp, const VectorXd &zmp )
+    void calculateBodyModification(const Vector3 &refZmp, const Vector3 &zmp)
     {
         // sets gains
-        const double xg_pgain = 2.0;
-        const double xg_dgain = 4.0;
-        const double yg_pgain = 0.1;
-        const double yg_dgain = 0.2;
+        const double Kx1 = 0.1;
+        const double Kx2 = 0.2;
+        const double Ky1 = 0.1;
+        const double Ky2 = 0.2;
 
         // calculates the (3-dimensional) ZMP error
-        const Vector3d errorZmp = refZmp - zmp;
+        const Vector3d errorZmp = zmp - refZmp;
 
         // calculates the reference body displacement from the ZMP error
-        bodyModification[0] = xg_pgain * errorZmp[0] + xg_dgain * dZmp[0];
-        bodyModification[1] = yg_pgain * errorZmp[1] + yg_dgain * dZmp[1];
-        dZmp[0] += bodyModification[0] * dt;
-        dZmp[1] += bodyModification[1] * dt;
+        double dx = Kx1 * errorZmp[0] - Kx2 * bodyModification[0];
+        double dy = Ky1 * errorZmp[1] - Ky2 * bodyModification[1];
+        bodyModification[0] += dx * dt;
+        bodyModification[1] += dy * dt;
 
         return;
     }
 
     void modifyFootPositions()
     {
-        Vector3 pr, pl;  // foot positions (relative)
-        Matrix3 Rr, Rl;  // foot orientations (relative)
-        bool isSuccess = true;
-
-        auto qref = qseq->frame(currentFrameIndex);
-        for(int i = 0; i < ikBody->numJoints(); ++i)
-        {
-            ikBody->joint(i)->q() = ioBody->joint(i)->q();
+        cnoid::MultiSeq<double>::Frame qref = qseq->frame(currentFrameIndex);
+        for (int i = 0; i < ikBody->numJoints(); ++i) {
+            ikBody->joint(i)->q() = qref[i];
         }
         baseToRAnkle->calcForwardKinematics();
         baseToLAnkle->calcForwardKinematics();
 
         // applies feedbacks
-        Isometry3 Tr = baseToRAnkle->baseLink()->T().inverse() * baseToRAnkle->endLink()->T();
+        Isometry3 Tr = baseToRAnkle->endLink()->T();
         Tr.translation() -= bodyModification;
-        Isometry3 Tl = baseToLAnkle->baseLink()->T().inverse() * baseToLAnkle->endLink()->T();
+        Isometry3 Tl = baseToLAnkle->endLink()->T();
         Tl.translation() -= bodyModification;
 
         // solves IK
+        bool isSuccess = true;
         isSuccess &= baseToRAnkle->calcInverseKinematics(Tr);
         isSuccess &= baseToLAnkle->calcInverseKinematics(Tl);
-        if(isSuccess)
-        {
-            for(int i = 0; i < baseToRAnkle->numJoints(); ++i)
-            {
-                Link *joint = baseToRAnkle->joint(i);
-                qref[joint->jointId()] = joint->q();
+
+        if (isSuccess) {
+            for (auto joint : baseToRAnkle->joints()) {
+                qref_modified[joint->jointId()] = joint->q();
             }
-            for(int i = 0; i < baseToLAnkle->numJoints(); ++i) 
-            {
-                Link *joint = baseToLAnkle->joint(i);
-                qref[joint->jointId()] = joint->q();
+            for (auto joint : baseToLAnkle->joints()) {
+                qref_modified[joint->jointId()] = joint->q();
             }
-            qseq->frame(currentFrameIndex) = qref;
         }
     }
 
@@ -190,28 +200,36 @@ public:
     {
         Vector3 zmp;
         const Vector6 fr_local = rf_sensor->F();
-        const Matrix3 rf_rotation = ioBody->link("RLEG_LINK5")->R() * rf_sensor->localRotation(); 
-        Vector6 fr; 
+        const Matrix3 rf_rotation = ioBody->link("RLEG_LINK5")->R()
+                                    * rf_sensor->localRotation();
+        Vector6 fr;
         fr << rf_rotation.transpose() * fr_local.block<3, 1>(0, 0),
-              rf_rotation.transpose() * fr_local.block<3, 1>(3, 0);
-        const Vector3 rf_position = ioBody->link("RLEG_LINK5")->p() + ioBody->link("RLEG_LINK5")->R() * rf_sensor->localTranslation();
+            rf_rotation.transpose() * fr_local.block<3, 1>(3, 0);
+        const Vector3 rf_position = ioBody->link("RLEG_LINK5")->p()
+                                    + ioBody->link("RLEG_LINK5")->R()
+                                          * rf_sensor->localTranslation();
 
         const Vector6 fl_local = lf_sensor->F();
-        const Matrix3 lf_rotation = ioBody->link("LLEG_LINK5")->R() * lf_sensor->localRotation(); 
+        const Matrix3 lf_rotation = ioBody->link("LLEG_LINK5")->R()
+                                    * lf_sensor->localRotation();
         Vector6 fl;
         fl << lf_rotation.transpose() * fl_local.block<3, 1>(0, 0),
-              lf_rotation.transpose() * fl_local.block<3, 1>(3, 0);
-        const Vector3 lf_position = ioBody->link("LLEG_LINK5")->p() + ioBody->link("LLEG_LINK5")->R() * lf_sensor->localTranslation();
+            lf_rotation.transpose() * fl_local.block<3, 1>(3, 0);
+        const Vector3 lf_position = ioBody->link("LLEG_LINK5")->p()
+                                    + ioBody->link("LLEG_LINK5")->R()
+                                          * lf_sensor->localTranslation();
 
-        if(fr[2] > 0.0 && fl[2] > 0.0) {
-            const double px = (rf_position[0] * fr[2] + lf_position[0] * fl[2]) / (fr[2] + fl[2]);
-            const double py = (rf_position[1] * fr[2] + lf_position[1] * fl[2]) / (fr[2] + fl[2]);
-            zmp << px, py, 0.0;
-            zmp += rf_sensor->localTranslation();
-        } else if(fr[2] > 0.0) {
+        if (fr[2] > 0.0 && fl[2] > 0.0) {
+            Vector3 zmpr, zmpl;
+            calcZMPfromWrench(fr, zmpr);
+            calcZMPfromWrench(fl, zmpl);
+
+            zmp = ((zmpr + rf_position) * fr[2] + (zmpl + lf_position) * fl[2])
+                  / (fr[2] + fl[2]);
+        } else if (fr[2] > 0.0) {
             calcZMPfromWrench(fr, zmp);
             zmp += rf_position;
-        } else if(fl[2] > 0.0) {
+        } else if (fl[2] > 0.0) {
             calcZMPfromWrench(fl, zmp);
             zmp += lf_position;
         } else {
@@ -222,30 +240,35 @@ public:
         calculateBodyModification(refZmp, zmp);
         modifyFootPositions();
 
-        auto qref = qseq->frame(currentFrameIndex);
-        for(int i=0; i < 37; ++i) {
+        for (int i = 0; i < ioBody->numJoints(); ++i) {
             const double q = ioBody->joint(i)->q();
-            const double dq = (q - q_old.at(i)) / dt;
-            const double dqref = (qref[i] - qref_old[i]) / dt;
-            const double u = (qref[i] - q) * pgain[i] + (dqref - dq) * dgain[i];
+            const double dq = (q - q_old[i]) / dt;
+            const double dqref = (qref_modified[i] - qref_old[i]) / dt;
+
+            // PD control
+            const double u = (qref_modified[i] - q) * pgain[i]
+                             + (dqref - dq) * dgain[i];
             ioBody->joint(i)->u() = u;
 
+            // record joint positions
             q_old[i] = q;
-            qref_old[i] = qref[i];
+            qref_old[i] = qref_modified[i];
         }
 
         bool isActive = false;
-        if(currentFrameIndex < qseq->numFrames()) {
+        if (currentFrameIndex < qseq->numFrames()) {
             ++currentFrameIndex;
             isActive = true;
         }
         return isActive;
     }
 
-    void calcZMPfromWrench(const Vector6& wrench, Vector3& zmp_out) {
-        const double d = 0.019;  // distance from the bottom of foot to the force sensor
-        if(wrench[2] > 0.0) {
-            const double px = - (wrench[4] + d * wrench[0]) / wrench[2];
+    void calcZMPfromWrench(const Vector6 &wrench, Vector3 &zmp_out)
+    {
+        const double d
+            = 0.019;  // distance from the bottom of foot to the force sensor
+        if (wrench[2] > 0.0) {
+            const double px = -(wrench[4] + d * wrench[0]) / wrench[2];
             const double py = (wrench[3] + d * wrench[1]) / wrench[2];
             zmp_out << px, py, 0.0;
         } else {
